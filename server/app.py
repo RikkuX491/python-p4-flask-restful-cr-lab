578d7675d30a7b63625d8c8d16fceb0324d4dc97
Ricardo Guerra <RikkuG491@gmail.com>
2023-06-14 12:16:09 -0400

    Added solution code

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import ipdb
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -17,10 +18,60 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    def get(self):
+        plants = Plant.query.all()
+        response_body = []
+        for plant in plants:
+            plant_dictionary = {
+                'id': plant.id,
+                'name': plant.name,
+                'image': plant.image,
+                'price': plant.price
+            }
+            response_body.append(plant_dictionary)
+
+        return make_response(jsonify(response_body), 200)
+    
+    def post(self):
+        new_plant = Plant(name=request.get_json().get('name'), image=request.get_json().get('image'), price=request.get_json().get('price'))
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_body = {
+            "id": new_plant.id,
+            "name": new_plant.name,
+            "image": new_plant.image,
+            "price": new_plant.price
+        }
+
+        return make_response(jsonify(response_body), 201)
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        if not plant:
+            response_body = {
+                "error": "Plant not found"
+            }
+            status = 404
+
+        else:
+            response_body = {
+                "id": plant.id,
+                "name": plant.name,
+                "image": plant.image,
+                "price": plant.price
+            }
+            status = 200
+
+        return make_response(jsonify(response_body), status)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
